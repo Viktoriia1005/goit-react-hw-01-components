35436a2663912718cad30c7743aa345cffff7d48
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-05-27 12:39:51 +0300

    done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,31 +5,31 @@ const Profile = ({username, tag, location, avatar, stats}) => {
     const { followers, views, likes } = stats;
     return (
         <div className={s.profile}>
-        <div className={s.description}>
-            <img
-            src={avatar}
-            alt={username}
-            className={s.avatar}
-            />
-            <p className={s.name}>{username}</p>
-            <p className={s.tag}>{tag}</p>
-            <p className={s.location}>{location}</p>
-        </div>
+            <div className={s.description}>
+                <img
+                src={avatar}
+                alt={username}
+                className={s.avatar}
+                />
+                <p className={s.name}>{username}</p>
+                <p className={s.tag}>{tag}</p>
+                <p className={s.location}>{location}</p>
+            </div>
 
-        <ul className={s.stats}>
-            <li>
-            <span className={s.label}>Followers</span>
-            <span className={s.quantity}>{followers}</span>
-            </li>
-            <li>
-            <span className={s.label}>Views</span>
-            <span className={s.quantity}>{views}</span>
-            </li>
-            <li>
-            <span className={s.label}>Likes</span>
-            <span className={s.quantity}>{likes}</span>
-            </li>
-        </ul>
+            <ul className={s.stats}>
+                <li>
+                <span className={s.label}>Followers</span>
+                <span className={s.quantity}>{followers}</span>
+                </li>
+                <li>
+                <span className={s.label}>Views</span>
+                <span className={s.quantity}>{views}</span>
+                </li>
+                <li>
+                <span className={s.label}>Likes</span>
+                <span className={s.quantity}>{likes}</span>
+                </li>
+            </ul>
         </div>
     )
 }
